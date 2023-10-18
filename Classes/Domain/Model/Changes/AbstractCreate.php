f574107f07dcd1a798774576d6cc125599df3cea
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2023-10-18 22:52:22 +0200

    TASK: NodeCreationCommands: Guarantee that the $tetheredDescendantNodeAggregateIds are generated beforehand

--- Classes/Domain/Model/Changes/AbstractCreate.php
+++ Classes/Domain/Model/Changes/AbstractCreate.php
@@ -132,7 +132,8 @@ abstract class AbstractCreate extends AbstractStructuralChange
 
         $commands = $this->applyNodeCreationHandlers(
             NodeCreationCommands::fromFirstCommand(
-                $command
+                $command,
+                $contentRepository->getNodeTypeManager()
             ),
             $nodeTypeName,
             $contentRepository
